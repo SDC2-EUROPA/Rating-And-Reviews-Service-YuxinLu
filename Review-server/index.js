acb91c7b7b67d491f4ca943284d84f4c2fd70d38
yuxinlu1 <huijialin0402@gmail.com>
2023-12-14 04:32:12 -0700

    dataimported

--- Review-server/index.js
+++ Review-server/index.js
@@ -8,13 +8,14 @@ require('dotenv').config();
 app.use(express.json());
 
 
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
     connectionLimit: 101,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
+
 
 
 // db.connect(err => {
